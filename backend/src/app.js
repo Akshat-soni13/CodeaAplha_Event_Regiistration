59ebc3134ad6dfa9be37c53589d07c932c19f152
AkshatSoni-13 <akisoni13@gmail.com>
2026-05-19 23:37:28 +0530

    NodeModule Deleted

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5175",
+  origin: process.env.Front_Url||"http://localhost:5173",
   credentials: true,
 }));
 
